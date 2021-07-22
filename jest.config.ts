532d92a53038fd444761b0063f9377c0f002988c
William Cooter <will@cooters.co.uk>
2021-07-22 18:23:01 +0100

    linting (#7)
    * README update
    
    * 100% ts
    
    * improved isNaughty logic
    
    * further improvements
    
    * readme update
    
    * linting

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['test.ts'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['test.ts'],
 };
